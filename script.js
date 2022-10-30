c37a0f39571146ef064d4a8dd2f74e442aa96396
Zayon Nieves <zayon.nieves1@gmail.com>
2022-10-30 11:08:11 -0400

    code update for web app functionality

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 
-function evaluate(guess, randomNum, count) {
+// display results
+function displayResult(results) {
+
+   // reference for old results message and the display area
+   let oldMessage = document.body.querySelector('p');
+   let msgDisplay = document.getElementById('result');
+
+   // if an old results message is already on the page, remove it
+   if(oldMessage) {
+      msgDisplay.removeChild(oldMessage);
+   }
+   // display the most recent results message on the page
+      msgDisplay.appendChild(results);
+}
+
+// compare user input to random number and give appropriate response
+function evaluate(guess) {
 
     let results;
 
@@ -23,21 +39,19 @@ function evaluate(guess, randomNum, count) {
      else {
         results = document.createElement('p');
         results.innerHTML = `
-        Random Number: ${randomNum}
-        Your guess: ${guess}
+        Random Number: ${randomNum} <br>
         You guessed correctly! You Win! 
         Attempts: ${attemptCount}`;
         results.style.color = 'mediumspringgreen';
      }
 
      // display results
-     document.getElementById('web-app').appendChild(results);
+     displayResult(results);
 }
 
-// method to link to submit button onclick property
+// validate the user input
 function validateInput() {
 
-    attemptCount++;
 
     // block scope variable: reference input box HTML element value
     let guess = document.getElementById('guess').value;
@@ -46,19 +60,17 @@ function validateInput() {
     if( guess === "" || guess.includes(".") || Number(guess) < 0 || Number(guess) > 100) {
 
         // if input is invalid, alert user, clear out input value and set cursor to input box
-        alert(`Invalid input. Try again. Attempts: ${attemptCount}`);
-        guess.value = "";
+        alert(`Invalid input. Try again.`);
+        document.getElementById('guess').value = "";
         document.getElementById('guess').focus();
     }
-
-    // if input is valid, compare it to the random number and respond accordingly
-    evaluate(Number(guess), randomNum, attemptCount);
-
-
-    // test code
-    //alert(guess);
-    //alert(typeof guess);
+    // if input is valid, count as an attempt and evaluate
+    else {
+      attemptCount++;
+      evaluate(Number(guess));
+    }
 }
+
 
 // global variables: reference submit button HTML element in JavaScript code
 let attemptCount = 0;
